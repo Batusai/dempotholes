6aa9f7e2b6c0db0dec5b3b996585946706e7a3bd
Developer's Air <developersair@Developers-MacBook-Air.local>
2012-12-28 16:34:32 -0430

    Added jQuery mobile wire frame
    Made several edits to the project so that it works like the jQuery
    mobile wireframe we did in class.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var AppRouter = Backbone.Router.extend({
 
     routes: {
-        ""                  : "list",
-        "wines/page/:page"	: "list",
+        ""                  : "home",
+        "wines"             : "list",
+        "wines/page/:page"  : "list",
         "wines/add"         : "addWine",
         "wines/:id"         : "wineDetails",
         "about"             : "about"
@@ -13,7 +14,15 @@ var AppRouter = Backbone.Router.extend({
         $('.header').html(this.headerView.el);
     },
 
-	list: function(page) {
+    home: function (id) {
+        if (!this.homeView) {
+            this.homeView = new HomeView();
+        }
+        $('#content').html(this.homeView.el);
+        this.headerView.selectMenuItem('home-menu');
+    },
+
+    list: function(page) {
         var p = page ? parseInt(page, 10) : 1;
         var wineList = new WineCollection();
         wineList.fetch({success: function(){
@@ -23,18 +32,18 @@ var AppRouter = Backbone.Router.extend({
     },
 
     wineDetails: function (id) {
-        var wine = new Wine({id: id});
+        var wine = new Wine({_id: id});
         wine.fetch({success: function(){
             $("#content").html(new WineView({model: wine}).el);
         }});
         this.headerView.selectMenuItem();
     },
 
-	addWine: function() {
+    addWine: function() {
         var wine = new Wine();
         $('#content').html(new WineView({model: wine}).el);
         this.headerView.selectMenuItem('add-menu');
-	},
+    },
 
     about: function () {
         if (!this.aboutView) {
@@ -46,7 +55,8 @@ var AppRouter = Backbone.Router.extend({
 
 });
 
-utils.loadTemplate(['HeaderView', 'WineView', 'WineListItemView', 'AboutView'], function() {
+utils.loadTemplate(['HomeView', 'HeaderView', 'WineView', 'WineListItemView', 'AboutView'], function() {
     app = new AppRouter();
+    Backbone.history.stop();
     Backbone.history.start();
 });
